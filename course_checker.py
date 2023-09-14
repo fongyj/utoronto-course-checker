d287aa2eb43b65120f774031ed1e4ffe55976245
Fong Yih Jie <fongyj@hotmail.com>
2023-09-14 16:30:56 -0400

    Refactor frequency of checking

--- course_checker.py
+++ course_checker.py
@@ -27,29 +27,31 @@ payload = {"courseCodeAndTitleProps":
 headers = {"Accept": "application/json"}
 
 notify = Notify()
+frequency = 60 # seconds
 
-while True:
-    for interested_course in interested_courses:
-        course_payload = dict(payload)
-        course_payload["courseCodeAndTitleProps"]["courseCode"] = interested_course["courseCode"]
-        course_payload["courseCodeAndTitleProps"]["courseTitle"] = interested_course["courseTitle"]
-        course_payload["divisions"] = interested_course["divisions"]
-        response = requests.post(url, json=course_payload, headers=headers)
-        response = response.json()
-        course = response["payload"]["pageableCourse"]["courses"][0]
-        sections = course["sections"]
-        target = interested_course["target"]
-        for section in sections:
-            message = course["code"] + " " + course["name"]
-            if target and section["name"] not in target:
-                continue
-            if section["type"] == "Lecture":
-                message += "\n" + section["name"]
-                availability = section["maxEnrolment"] - section["currentEnrolment"]
-                waitlist = section["currentWaitlist"]
-                message += " Availability: {}".format(availability)
-                if availability > 0:
-                    print(message)
-                    print()
-                    notify.send(message)
-    time.sleep(5)
+if __name__ == "__main__":
+    while True:
+        for interested_course in interested_courses:
+            course_payload = dict(payload)
+            course_payload["courseCodeAndTitleProps"]["courseCode"] = interested_course["courseCode"]
+            course_payload["courseCodeAndTitleProps"]["courseTitle"] = interested_course["courseTitle"]
+            course_payload["divisions"] = interested_course["divisions"]
+            response = requests.post(url, json=course_payload, headers=headers)
+            response = response.json()
+            course = response["payload"]["pageableCourse"]["courses"][0]
+            sections = course["sections"]
+            target = interested_course["target"]
+            for section in sections:
+                message = course["code"] + " " + course["name"]
+                if target and section["name"] not in target:
+                    continue
+                if section["type"] == "Lecture":
+                    message += "\n" + section["name"]
+                    availability = section["maxEnrolment"] - section["currentEnrolment"]
+                    # waitlist = section["currentWaitlist"]
+                    message += " Availability: {}".format(availability)
+                    if availability > 0:
+                        print(message)
+                        print()
+                        notify.send(message)
+        time.sleep(frequency)
